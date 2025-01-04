df34c5c009594472b23ce48881bac5cc6fec2314
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-04 23:38:52 +0600

    add my-booking and manage-bookings pages

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -14,6 +14,8 @@ import Profile from "../pages/Dashboard/Common/Profile";
 import ManageUsers from "../pages/Dashboard/Admin/ManageUser";
 import HostRoute from "./HostRoute";
 import AdminRoute from "./AdminRoute";
+import MyBookings from "../pages/Dashboard/Guest/MyBookings";
+import ManageBookings from "../pages/Dashboard/Host/ManageBookings";
 
 export const router = createBrowserRouter([
   {
@@ -81,6 +83,24 @@ export const router = createBrowserRouter([
             <AdminRoute>
               <ManageUsers />
             </AdminRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "my-bookings",
+        element: (
+          <PrivateRoute>
+            <MyBookings />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "manage-bookings",
+        element: (
+          <PrivateRoute>
+            <HostRoute>
+              <ManageBookings />
+            </HostRoute>
           </PrivateRoute>
         ),
       },
